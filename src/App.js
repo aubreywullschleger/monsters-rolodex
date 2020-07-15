6b142648d02ef6a8d1c6c9ce7d6c907c611ffa0d
aubreywullschleger <aubreywullschleger@users.noreply.github.com>
2020-07-15 15:22:05 -0500

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
 import React from "react"
-import logo from "./logo.svg"
 import "./App.css"
 
 class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      monsters: [
-        {
-          name: "Frankenstein",
-          id: "asdf",
-        },
-        {
-          name: "Dracula",
-          id: "fdsa",
-        },
-        {
-          name: "Zombie",
-          id: "hjkl",
-        },
-      ],
+      monsters: []
     }
   }
+
+  componentDidMount() {
+    fetch("http://jsonplaceholder.typicode.com/users")
+    .then(response => response.json())
+    .then(users => this.setState({monsters: users}))
+  }
+
   render() {
     return (
       <div className="App">
